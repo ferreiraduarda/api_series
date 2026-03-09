5b53614ff7136cfea79d6bbea227454096240389
ferreiraduarda <maria.e.santos432@aluno.senai.br>
2026-03-09 16:43:04 -0300

    criação de rotas

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
-def main():
-    print("Hello from api-series!")
+from typing import Optional
 
+from fastapi import FastAPI
 
-if __name__ == "__main__":
-    main()
+app = FastAPI()
+
+@app.get("/")
+async def read_roote():
+    return {"mensagem": "Olá, mundo!"}
+
+@app.get("/itens/{item_id}")
+async def read_item(item_id: int, q: Optional[str] = None):
+    return {"item_id": item_id, "q": q}
+
+@app.get("/soma")
+async def soma(a: int, b: int):
+    return {"resultado": a + b}
+
+@app.get("/adicao/{a}/{b}")
+async def adicao(a: int, b: int):
+    return {"resultado": a + b}
